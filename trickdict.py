6e00c0987e0d04530bb6fbfd8445cd90f050afb1
MUKESH <mukesh.ora@outlook.com>
2019-08-29 00:02:59 +0530

    one more trick added

--- trickdict.py
+++ trickdict.py
@@ -1,4 +1,11 @@
 # sorting a dict Key
 a_dict = {'banana':500,'apple':200,'grape':250}
-new_dict = {k : a_dict[k]  for k in sorted(a_dict)}
-print(new_dict)
+# new_dict = {k : a_dict[k]  for k in sorted(a_dict)}
+# print(new_dict)
+
+# sum of values
+total_incm = 0.00
+for value in a_dict.values():
+    total_incm += value
+print(total_incm)
+
