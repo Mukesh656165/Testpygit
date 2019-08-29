d386a31cf96be6d887711e621e560e03151499f5
MUKESH <mukesh.ora@outlook.com>
2019-08-29 17:16:47 +0530

    one code added

--- trickdict2.py
+++ trickdict2.py
@@ -1,1 +1,14 @@
 # Welcome to dictionary tricks 2
+
+# how to remove items using pop method
+
+a_dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
+while True:
+    try:
+        print(f'Dictionary length: {len(a_dict)}')
+        item = a_dict.popitem()
+        # Do something with item here...
+        print(f'{item} removed')
+    except KeyError:
+        print('The dictionary has no item now...')
+        break
