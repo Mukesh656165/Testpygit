91427ede1c8c02f95604641a7534d82a3041c2a4
MUKESH <mukesh.ora@outlook.com>
2019-08-31 04:17:02 +0530

    no line addde by Mukesh

--- trickdict2.py
+++ trickdict2.py
@@ -2,16 +2,16 @@
 
 # how to remove items using pop method
 
-# a_dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
-# while True:
-#     try:
-#         print(f'Dictionary length: {len(a_dict)}')
-#         item = a_dict.popitem()
-#         # Do something with item here...
-#         print(f'{item} removed')
-#     except KeyError:
-#         print('The dictionary has no item now...')
-#         break
+a_dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
+while True:
+    try:
+        print(f'Dictionary length: {len(a_dict)}')
+        item = a_dict.popitem()
+        # Do something with item here...
+        print(f'{item} removed')
+    except KeyError:
+        print('The dictionary has no item now...')
+        break
 
 # to iterate both the dictionary
 from collections import ChainMap
