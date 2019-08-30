203062ca082b17f52b9b72a92c703c11b4114c38
MUKESH <mukesh.ora@outlook.com>
2019-08-31 04:18:57 +0530

    no line addde by Mukesh

--- trickdict3.py
+++ trickdict3.py
@@ -1,27 +1,27 @@
 # Hey this is new file for python dict
 # #building a dictionary incrementally
-# person = {}
-# person['Name'] = 'Jason'
-# person['lname']= 'Bourne'
-# person['age']= 36
-# person['spouse'] = 'Anna'
-# person['childrn']= ['Matt','Annie']
-# person['pets'] ={'dog':'Germanshepherd','cat':'bree'}
-# print(person)
+person = {}
+person['Name'] = 'Jason'
+person['lname']= 'Bourne'
+person['age']= 36
+person['spouse'] = 'Anna'
+person['childrn']= ['Matt','Annie']
+person['pets'] ={'dog':'Germanshepherd','cat':'bree'}
+print(person)
 #
 # # How to use Get method in nested dictionary
-# a = person.get('pets').get('ak','not exist')
-# print(a)
+a = person.get('pets').get('ak','not exist')
+print(a)
 #
 # # a tuple can also used to a dictionary key
-# a_dict = {(1,1):'Mukesh',(2,2): 'vaibhav'}
-# print(a_dict)
-# print(a_dict[(1,1)])
+a_dict = {(1,1):'Mukesh',(2,2): 'vaibhav'}
+print(a_dict)
+print(a_dict[(1,1)])
 #
 # # A list should not be a key in dictionary
 #
-# b_dict = {[1,2]:'mukesh'}
-# print(b_dict)
+b_dict = {[1,2]:'mukesh'}
+print(b_dict)
 # note U should pass a hashable object as key (hashable ---> immutable)
 a = (1,2)
 print(type(a))
